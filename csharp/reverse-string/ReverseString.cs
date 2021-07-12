1cde00ba38f61af27ee356f902bc1bc310c62b80
Quinten <me@quintenjames.com>
2021-07-12 01:00:17 -0600

    Reverse String Exercise Complete

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
