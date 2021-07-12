6f6e4af9ad266bca2193a6aadd11f7741d4f5bd2
Quinten <me@quintenjames.com>
2021-07-12 11:56:34 -0600

    Leap Year Solved
    Basic if else and one line Conditional Operatior

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,6 +4,15 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? true : false;
+        //if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        //{
+        //    return true;
+        //}
+        //else
+        //{
+        //    return false;
+        //}
     }
 }
