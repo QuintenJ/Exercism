d60b2f0259c24066330b20fa6bace91ba8036647
Quinten <me@quintenjames.com>
2021-07-13 14:32:27 -0600

    Resistor Color Solved

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,13 +2,7 @@
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public static string[] Colors()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private static readonly string[] ResistorColors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    public static int ColorCode(string color) => Array.IndexOf(ResistorColors, color);
+    public static string[] Colors() => ResistorColors;
 }
