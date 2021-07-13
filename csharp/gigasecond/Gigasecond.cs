813d09dcc0dd0483104b3837997c619d5975917b
Quinten <me@quintenjames.com>
2021-07-13 12:03:09 -0600

    Gigasecond Solved

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,8 +2,6 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(Math.Pow(10, 9));
+
 }
