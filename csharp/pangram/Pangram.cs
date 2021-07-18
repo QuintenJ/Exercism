cb5e8f2f3f08b76c12c88624e04065b121a6f145
Quinten <me@quintenjames.com>
2021-07-17 23:17:38 -0600

    Pangram Solve

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,8 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool IsPangram(string input) => input.ToLower().Where(ch => Char.IsLetter(ch)).GroupBy(ch => ch).Count() == 26;
+
 }
