691338af630336018da4a644b92afc83dd6c478b
Quinten <me@quintenjames.com>
2021-07-25 22:20:48 -0600

    RNA Transcription Exercise Solve

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -4,6 +4,15 @@ public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] dna = { 'A', 'C', 'G', 'T' };
+        char[] rna = { 'U', 'G', 'C', 'A' };
+        string rnaStrand = "";
+
+        for (int i = 0; i <= nucleotide.Length - 1; i++)
+        {
+            rnaStrand += rna[Array.IndexOf(dna, nucleotide[i])];
+        }
+
+        return rnaStrand;
     }
 }
