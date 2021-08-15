c97afe95153cb35b2af017e556d559082495607b
Quinten <me@quintenjames.com>
2021-08-15 00:53:36 -0600

    High Scores Exercism Solved All Test Pass

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -2,30 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+/*\
+Manage a game player's High Score list.
+
+Your task is to build a high-score component of the classic Frogger game, 
+one of the highest selling and addictive games of all time, and a classic of the arcade era.
+ Your task is to write methods that return the highest score from the list, the last added score and the three highest scores.
+*/
 public class HighScores
 {
+    private List<int> highScoresList;
+
     public HighScores(List<int> list)
     {
-        throw new NotImplementedException();
+        highScoresList = list;
     }
 
-    public List<int> Scores()
-    {
-        throw new NotImplementedException();
-    }
+    public List<int> Scores() => highScoresList;
 
-    public int Latest()
-    {
-        throw new NotImplementedException();
-    }
 
-    public int PersonalBest()
-    {
-        throw new NotImplementedException();
-    }
+    public int Latest() => highScoresList.Last();
 
-    public List<int> PersonalTopThree()
-    {
-        throw new NotImplementedException();
-    }
+
+    public int PersonalBest() => highScoresList.Max();
+
+
+    public List<int> PersonalTopThree() => highScoresList.OrderByDescending(i => i).Take(3).ToList();
+
 }
