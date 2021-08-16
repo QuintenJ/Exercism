bcc6d8c80cd4bc5d9b9aefcf4888a3b6d7708c18
Quinten <me@quintenjames.com>
2021-08-16 00:49:00 -0600

    Hamming Exercism Solved All Test Passing

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,6 @@
 using System;
-
+using System.Linq;
 public static class Hamming
 {
-    public static int Distance(string firstStrand, string secondStrand)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int Distance(string firstStrand, string secondStrand) => firstStrand.Length != secondStrand.Length ? throw new ArgumentException("The length of strings are not equal.") : firstStrand.Where((l, i) => secondStrand[i] != l).Count();
 }
