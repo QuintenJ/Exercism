7c90d8e15664f7f631c62cc6758d25dfe4b985e5
Quinten <me@quintenjames.com>
2021-08-16 01:23:22 -0600

    Nucleotide Count Exercism Solved All Test Passing

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,6 +5,18 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IDictionary<char, int> result = new Dictionary<char, int>() {
+            {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}
+        };
+
+        foreach (char occurs in sequence)
+        {
+            if (!result.ContainsKey(occurs))
+            {
+                throw new ArgumentException("Is not nucleotide.");
+            }
+            result[occurs]++;
+        }
+        return result;
     }
 }
