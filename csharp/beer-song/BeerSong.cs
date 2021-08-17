7cac940bbfe8ad75546413443fccf76a487b368f
Quinten <me@quintenjames.com>
2021-08-17 00:50:29 -0600

    Beer Song Exercism Solved All Test Passing

--- csharp/beer-song/BeerSong.cs
+++ csharp/beer-song/BeerSong.cs
@@ -1,9 +1,26 @@
 ﻿using System;
-
+using System.Linq;
 public static class BeerSong
 {
+    private static string NumberToString(this int number) => number > 0 ? number.ToString() : "No more";
+    private static string BottleText(this int number) => number != 1 ? "bottles" : "bottle";
     public static string Recite(int startBottles, int takeDown)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return string.Join("" + '\n' + '\n',
+            Enumerable.Range(0, takeDown)
+                      .Select(x => startBottles - x)
+                      .Select(b =>
+                      {
+                          var current = b;
+                          var next = b - 1;
+
+                          var verse = $"{current.NumberToString()} {current.BottleText()} of beer on the wall, {current.NumberToString().ToLower()} {current.BottleText()} of beer.\n";
+
+                          verse += (next >= 0) ? $"Take {(current == 1 ? "it" : "one")} down and pass it around, {next.NumberToString().ToLower()} {next.BottleText()} of beer on the wall."
+                                               : $"Go to the store and buy some more, 99 {99.BottleText()} of beer on the wall.";
+
+                          return verse;
+                      })
+                      .ToArray());
     }
 }
