4037e15083d6ae486269aef8c72ad0584bdb8f82
Quinten <me@quintenjames.com>
2021-08-22 00:08:43 -0600

    Grains Exercism Solved All Test Passing

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -4,11 +4,14 @@ public static class Grains
 {
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (n < 1 || n > 64)
+            throw new ArgumentOutOfRangeException("Value must be between 1 and 64");
+
+        return (ulong)Math.Pow(2, n - 1);
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (ulong)Math.Pow(2, 64) - 1;
     }
 }
