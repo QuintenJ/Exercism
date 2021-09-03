692a76b5e4842c05ccda28a67cd390023b8da44c
Quinten <me@quintenjames.com>
2021-09-03 15:43:48 -0600

    Exercism Cars Assemble Solved All Tests Passing

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -4,11 +4,48 @@ static class AssemblyLine
 {
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        if (speed >= 1 && speed <= 4)
+        {
+            return speed * 221.0;
+        }
+        else if (speed > 5 && speed <= 7)
+        {
+            return speed * 198.9;
+        }
+        else if (speed == 9)
+        {
+            return speed * 176.8;
+        }
+        else if (speed == 10)
+        {
+            return Math.Round(speed * 170.17, 2);
+        }
+        {
+            return speed * 0.0;
+        }
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        if (speed >= 1 && speed <= 4)
+        {
+            return (speed * 221) / 60;
+        }
+        else if (speed >= 5 && speed <= 8)
+        {
+            return (speed * 199) / 60;
+        }
+        else if (speed == 9)
+        {
+            return speed * 177 / 60;
+        }
+        else if (speed == 10)
+        {
+            return speed * 170 / 60;
+        }
+        {
+            return speed * 0;
+        }
     }
 }
+
