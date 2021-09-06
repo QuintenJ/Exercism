14c1690f97ef7478bad665abb50707e7813254bc
Quinten <me@quintenjames.com>
2021-09-06 00:02:39 -0600

    Exercism Squeaky Clean Solved All Tests Passing

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -4,6 +4,27 @@ public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        var result = "";
+        var toUpperCase = false;
+
+        foreach (var c in identifier)
+        {
+            if (c == ' ')
+                result += "_";
+            else if (c < 13)
+                result += "CTRL";
+            else if (c >= 'α' && c <= 'ω')
+                continue;
+            else if (c == '-')
+                toUpperCase = true;
+            else if (toUpperCase)
+            {
+                result += char.ToUpper(c);
+                toUpperCase = false;
+            }
+            else if (char.IsLetter(c))
+                result += c;
+        }
+        return result;
     }
 }
