c120d926761d802d851c22b5fa8dadfec0e6c967
Quinten <me@quintenjames.com>
2021-09-09 22:03:52 -0600

    Exercism Booking Up For Beauty Refactor
    Local test pass with previous version failed on exercism submit. This fixed the exercism submit tests

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -12,9 +12,9 @@ static class Appointment
             return false;
     }
 
-    public static bool IsAfternoonAppointment(DateTime appointmentDate) => (appointmentDate.Hour >= 12 && appointmentDate.Hour < 18);
+    public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
 
-    public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString()}.";
+    public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString("M/d/yyyy h:mm:ss tt")}.";
 
-    public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15);
+    public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
 }
