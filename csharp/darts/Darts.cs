0db040962b7994871116fcfd0feb8c27200b3895
Quinten <me@quintenjames.com>
2021-09-12 00:36:45 -0600

    Exercism Darts Solved All Tests Passing

--- csharp/darts/Darts.cs
+++ csharp/darts/Darts.cs
@@ -4,6 +4,15 @@ public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        var diststanceFromCenter = (x * x) + (y * y);
+
+        if (diststanceFromCenter <= 1)
+            return 10;
+        else if (diststanceFromCenter > 1 && diststanceFromCenter <= 25)
+            return 5;
+        else if (diststanceFromCenter > 25 && diststanceFromCenter <= 100)
+            return 1;
+        else
+            return 0;
     }
 }
