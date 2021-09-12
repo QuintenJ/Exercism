b2eec8837f3975d159d73fc34b59b7b50446670c
Quinten <me@quintenjames.com>
2021-09-11 23:19:37 -0600

    Exercism Need For Speed Solved All Tests Passing

--- csharp/need-for-speed/NeedForSpeed.cs
+++ csharp/need-for-speed/NeedForSpeed.cs
@@ -2,35 +2,55 @@ using System;
 
 class RemoteControlCar
 {
-    // TODO: define the constructor for the 'RemoteControlCar' class
+    private readonly int speed;
+    private readonly int batteryDrain;
+    int distance = 0;
+    int battery = 100;
 
-    public bool BatteryDrained()
+    public RemoteControlCar(int speed, int batteryDrain)
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
 
-    public int DistanceDriven()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
-    }
+    public bool BatteryDrained() => battery <= 0;
+
+    public int DistanceDriven() => distance;
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (BatteryDrained())
+            return;
+        distance += speed;
+        battery -= batteryDrain;
     }
 
-    public static RemoteControlCar Nitro()
-    {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
-    }
+    public static RemoteControlCar Nitro() => new RemoteControlCar(50, 4);
 }
 
 class RaceTrack
 {
-    // TODO: define the constructor for the 'RaceTrack' class
+    private readonly int distance;
+
+    public RaceTrack(int distance)
+    {
+        this.distance = distance;
+    }
+
+
 
     public bool CarCanFinish(RemoteControlCar car)
     {
-        throw new NotImplementedException("Please implement the RaceTrack.CarCanFinish() method");
+        bool canFinish = true;
+        bool finish = true;
+        while (finish)
+        {
+            car.Drive();
+            if (car.DistanceDriven() >= this.distance)
+                return canFinish = true;
+            if (car.BatteryDrained())
+                return canFinish = false;
+        }
+        return canFinish;
     }
 }
