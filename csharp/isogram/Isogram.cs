3770f63453be7f5eb8506b9dea0ce715e1ca1fb7
Quinten <me@quintenjames.com>
2021-09-29 22:57:29 -0600

    Exercism Isogram Pull

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,27 +1,9 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        List<char> results = new List<char>();
-        char[] letters = word.ToLower().ToCharArray();
-
-        foreach (char letter in letters)
-        {
-            // Allow whitespaces and hyphens to appear multiple times
-            if (Char.IsWhiteSpace(letter) || letter.Equals('-'))
-                continue;
-
-            // Check if the letter appeared already
-            if (results.Contains(letter))
-                return false;
-            else
-                results.Add(letter);
-        }
-
-        return true;
+        throw new NotImplementedException("You need to implement this function.");
     }
 }
