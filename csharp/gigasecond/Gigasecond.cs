294ea76020e2d60c2ddc123c6d2cf02d355e6c77
Quinten <me@quintenjames.com>
2021-10-15 00:15:46 -0600

    Exercism Gigasecond Pull

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,6 +2,8 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment) => moment.AddSeconds(1e9);
-
+    public static DateTime Add(DateTime moment)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
 }
