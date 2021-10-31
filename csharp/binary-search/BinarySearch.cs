20e21d83909502d8f42899941e94c9cbe4c8d735
Quinten <me@quintenjames.com>
2021-10-31 02:33:04 -0600

    Exercism Binary Search Solved All Tests Passing

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -4,6 +4,24 @@ public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int low = 0;
+        int high = input.Length - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (input[mid] == value)
+            {
+                return mid;
+            }
+            else if (input[mid] < value)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 }
