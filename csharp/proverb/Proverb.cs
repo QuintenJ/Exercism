e08b2a1408e7860a24f42f3178ebf26528edf10c
Quinten <me@quintenjames.com>
2021-10-31 02:41:33 -0600

    Exercism Proverb Solved All Tests Passing

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        string[] result = new string[subjects.Length];
+        for (int i = 0; i < subjects.Length; i++)
+        {
+            if (i == subjects.Length - 1)
+            {
+                result[i] = $"And all for the want of a {subjects[0]}.";
+            }
+            else
+            {
+                result[i] = $"For want of a {subjects[i]} the {subjects[i + 1]} was lost.";
+            }
+        }
+        return result;
+    }
+}
