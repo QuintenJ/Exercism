de4b2a0d2aa114fdd1464466925fd12bcec84393
Quinten <me@quintenjames.com>
2022-01-19 19:43:07 -0700

    Exercism Nullability Tim Marketing Pull

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -3,6 +3,10 @@ using System;
 public static class LogAnalysis 
 {
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string input)
+    {
+        return input.Substring(0, input[1]);
+    }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
     
